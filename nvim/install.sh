e9327a65ba0ee521de2d5f7adcd82c806e7cea6c
eduardoguedes <eduardo.gomero@fbm.pt>
2025-02-09 20:33:09 +0000

    add comments to shell script

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "Downloading latest Neovim release..."
 sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+echo "Removing old nvim installation..."
 sudo rm -rf /opt/nvim
+echo "Unziping nvim tar file..."
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo "Removing tar file..."
 sudo rm nvim-linux-x86_64.tar.gz
+echo "Exporting nvim to path..."
 echo export PATH="$PATH:/opt/nvim-linux-x86_64/bin" >> ~/.zshrc
