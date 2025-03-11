accb2524dc5b4f4a88ecfb451ba6e77c6ad00697
eduardoguedes <eduardo.gomero@fbm.pt>
2025-03-11 12:10:07 +0000

    feat: add different architechtures install

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
+ARCH=$(uname -m)
+echo "Detected architecture: $ARCH"
+
+if [[ "$ARCH" == "x86_64" ]]; then
+  DOWNLOAD_URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+  EXTRACTED_DIR="nvim-linux-x86_64"
+  TARBALL="nvim-linux-x86_64.tar.gz"
+elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
+  DOWNLOAD_URL="https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz"
+  EXTRACTED_DIR="nvim-linux-arm64"
+  TARBALL="nvim-linux-arm64.tar.gz"
+else
+  echo "Unsupported architecture: $ARCH"
+  echo "This script supports x86_64 and ARM64 (aarch64) architectures."
+  exit 1
+fi
+
 set -e
 
-echo "Downloading latest Neovim release..."
-sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+echo "Downloading latest Neovim release for $ARCH..."
+sudo curl -LO "$DOWNLOAD_URL"
 
 echo "Removing old Neovim installation..."
 sudo rm -rf /opt/nvim
 
 echo "Extracting Neovim tar file..."
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+sudo tar -C /opt -xzf "$TARBALL"
+sudo mv "/opt/$EXTRACTED_DIR" /opt/nvim
 
 echo "Cleaning installation file..."
-sudo rm nvim-linux-x86_64.tar.gz
+sudo rm "$TARBALL"
 
 echo "Updating PATH..."
 if ! grep -q "/opt/nvim/bin" ~/.zshrc; then
-    echo 'export PATH="/opt/nvim/bin:$PATH"' >> ~/.zshrc
+  echo 'export PATH="/opt/nvim/bin:$PATH"' >>~/.zshrc
 fi
 
 echo "Installation complete!"
--- nvim/lua/guedes/default.lua
+++ nvim/lua/guedes/default.lua
@@ -1,6 +1,6 @@
 vim.opt.compatible = false
 vim.opt.hlsearch = true
-vim.opt.number = true         -- Show line numbers
+vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 vim.opt.laststatus = 2
 vim.opt.vb = true
@@ -19,17 +19,76 @@ vim.opt.shiftwidth = 3
 
 -- filetype related
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "gitcommit" },
-    callback = function(ev)
-        vim.api.nvim_set_option_value("textwidth", 72, { scope = "local" })
-    end
+	pattern = { "gitcommit" },
+	callback = function(ev)
+		vim.api.nvim_set_option_value("textwidth", 72, { scope = "local" })
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown" },
-    callback = function(ev)
-        vim.api.nvim_set_option_value("textwidth", 0, { scope = "local" })
-        vim.api.nvim_set_option_value("wrapmargin", 0, { scope = "local" })
-        vim.api.nvim_set_option_value("linebreak", 0, { scope = "local" })
-    end
+	pattern = { "markdown" },
+	callback = function(ev)
+		vim.api.nvim_set_option_value("textwidth", 0, { scope = "local" })
+		vim.api.nvim_set_option_value("wrapmargin", 0, { scope = "local" })
+		vim.api.nvim_set_option_value("linebreak", 0, { scope = "local" })
+	end,
 })
+
+-- BARBAR MAPPINGS
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", opts)
+map("n", "<A-.>", "<Cmd>BufferNext<CR>", opts)
+
+-- Re-order to previous/next
+map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
+
+-- Goto buffer in position...
+map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+
+-- Pin/unpin buffer
+map("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
+
+-- Goto pinned/unpinned buffer
+--                 :BufferGotoPinned
+--                 :BufferGotoUnpinned
+
+-- Close buffer
+map("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
+
+-- Wipeout buffer
+--                 :BufferWipeout
+
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+
+-- Magic buffer-picking mode
+map("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
+map("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", opts)
+
+-- Sort automatically by...
+map("n", "<Space>bb", "<Cmd>BufferOrderByBufferNumber<CR>", opts)
+map("n", "<Space>bn", "<Cmd>BufferOrderByName<CR>", opts)
+map("n", "<Space>bd", "<Cmd>BufferOrderByDirectory<CR>", opts)
+map("n", "<Space>bl", "<Cmd>BufferOrderByLanguage<CR>", opts)
+map("n", "<Space>bw", "<Cmd>BufferOrderByWindowNumber<CR>", opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
